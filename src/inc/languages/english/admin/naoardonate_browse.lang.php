24822ba43a0a5f87dcbeec0d5cb680dba9a55b3f
CoderMe <srcii@coderme.com>
2017-09-05 06:49:44 +0200

    Updating: src/inc/languages/english/admin/naoardonate_browse.lang.php
    Signed-off-by: CoderMe <srcii@coderme.com>

---
+++ src/inc/languages/english/admin/naoardonate_browse.lang.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ *
+ * CoderMe Donation FREE
+ * Copyright (c) CoderMe.com, All Rights Reserved
+ *
+ * Website: https://coderme.com
+ * Home:    https://coderme.com/mybb-donation-plugin
+ * License: https://coderme.com/mybb-donation-plugin#license
+ * Version: 6.0.0
+ *
+ **/
+
+
+
+
+$l['naoardonate_browse_unconfirmed'] = "Unconfirmed Only";
+$l['naoardonate_browse_confirmed'] = "Confirmed Donations Only";
+$l['naoardonate_browse_bannedunconfirmed'] = "Banned Unconfirmed Only";
+$l['naoardonate_browse_bannedconfirmed'] = "Banned Confirmed Only";
+$l['naoardonate_browse_all'] = "All Donations";
+
+$l['naoardonate_browse_unconfirmed_desc'] = "Donations that have not been confirmed yet, contain those donations paid by members and guests";
+$l['naoardonate_browse_confirmed_desc'] = "Donations that have been already confirmed , contain those donations paid by members and guests";
+
+
+$l['naoardonate_browse_bannedconfirmed_desc'] = "Banned users' Donations  that have been confirmed.";
+$l['naoardonate_browse_bannedunconfirmed_desc'] = "Banned users' Donations that have not been confirmed yet.";
+
+$l['naoardonate_browse_all_desc'] = "All Donations which are saved in Database, both confirmed and not confirmed included.";
+$l['naoardonate_browse_inline_nodonors_selected'] = "Sorry, but you did not select any donors. Please select some donors and try again.";
+$l['naoardonate_browse_inline_unconfirmed'] = '{1} donation(s) unconfirmed successfully';
+$l['naoardonate_browse_inline_unconfirmedmove'] = '{1} donation(s) unconfirmed and {2} donor(s) thrown out of donors group successfully';
+$l['naoardonate_browse_inline_deleted'] = '{1} donation(s) deleted successfully';
+$l['naoardonate_browse_inline_deletedmove'] = '{1} donation(s) deleted and {2} donor(s) thrown out of donors group successfully';
+$l['naoardonate_browse_inline_confirmed'] = '{1} donation(s) confirmed successfully';
+$l['naoardonate_browse_inline_confirmedmove'] = '{1} donation(s) confirmed and {2} donor(s) added to donors group successfully';
+$l['naoardonate_browse_delete'] = "Delete Donation(s)";
+$l['naoardonate_browse_ogid'] = "Original Group: ";
+$l['naoardonate_browse_unconfirm'] = "Unconfirm Donation(s)";
+$l['naoardonate_browse_confirm'] = "Confirm Donation(s)";
+$l['naoardonate_browse_clear'] = "Clear";
+$l['naoardonate_browse_inline_moderation'] = "Inline donor moderation:";
+
+?>
+
