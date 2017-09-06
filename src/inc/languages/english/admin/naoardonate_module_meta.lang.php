703b638397a45dc7d48ddfd51e9164a0b3607c26
CoderMe <srcii@coderme.com>
2017-09-06 07:04:52 +0200

    Updating: src/inc/languages/english/admin/naoardonate_module_meta.lang.php
    Signed-off-by: CoderMe <srcii@coderme.com>

---
+++ src/inc/languages/english/admin/naoardonate_module_meta.lang.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ *
+ * CoderMe Donation FREE
+ * Copyright (c) CoderMe.com, All Rights Reserved
+ *
+ * Website: https://coderme.com
+ * Home:    https://coderme.com/mybb-donation-plugin
+ * License: https://coderme.com/mybb-donation-plugin#license
+ * Version: 6.0.0
+ *
+ **/
+
+
+
+
+$l['naoardonate_meta_donors'] = "Donors";
+$l['naoardonate_meta_can_manage_donors'] = "Can browse and manage donors?";
+$l['naoardonate_meta_can_view_donations_stats'] = "Can view donations Statistics?";
+
+
+?>
+
