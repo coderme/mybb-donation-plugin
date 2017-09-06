100155dda4a68071c2df0902d19d092bb40484fa
CoderMe <srcii@coderme.com>
2017-09-06 07:04:52 +0200

    Updating: src/inc/languages/english/admin/naoardonate_plugin.lang.php
    Signed-off-by: CoderMe <srcii@coderme.com>

---
+++ src/inc/languages/english/admin/naoardonate_plugin.lang.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ *
+ * CoderMe Donation FREE
+ * Copyright (c) CoderMe.com, All Rights Reserved
+ *
+ * Website: https://coderme.com
+ * Home:    https://coderme.com/mybb-donation-plugin
+ * License: https://coderme.com/mybb-donation-plugin#license
+ * Version: 6.0.0
+ *
+ **/
+
+
+
+
+$l['naoardonate_plugin_description'] = <<<'DOC'
+This is the <b>FREE</b> version of CoderMe donation. <br />
+Features with mark <b style="background-color: rgba(255, 215, 0, 0.51); padding: .1rem .4rem; border-radius: 7px; line-height: 1.5rem; border: 1px solid;">GOLD</b> are <em>only</em> available in the <b>GOLD</b> version. <br />
+To learn more about the GOLD version please visit <a href="https://coderme.com/mybb-donation-gold" target="_blank" rel="noopener">this page</a>
+
+<h4 style="color: #FFF; background-color: rgb(0, 102, 162); padding: 0.3rem 0.8rem; border: 1px solid #CCC; border-radius:7px; box-shadow: 3px 3px 3px #CCC; background-image: linear-gradient(45deg,rgba(255,255,255,.15) 25%,transparent 25%,transparent 50%,rgba(255,255,255,.15) 50%,rgba(255,255,255,.15) 75%,transparent 75%,transparent); background-size: 40px 40px;">Features:</h4>
+<ul style="line-height: 1.5rem;">
+      <li><b style="background-color: rgba(255, 215, 0, 0.51); padding: .1rem .4rem; border-radius: 7px; line-height: 1.5rem; border: 1px solid;">GOLD</b>
+Accept Bitcoins, Litecoins and other Cryptocurrencies through <a href="https://localbitcoins.net/accounts/api/?ch=podn" target="_blank" rel="noopener">Localbitcoins</a> and <a href="https://www.coinpayments.net/index.php?ref=37b8a837b47e9181ac3ee9bcb92c5cfe" target="_blank" rel="noopener">Coinpayments</a></li>
+
+      <li>Accept Donations Online through Paypal and 2checkout</li>
+
+      <li>Accept Donations Offline through Western Union and Bank transfer</li>
+
+      <li><b style="background-color: rgba(255, 215, 0, 0.51); padding: .1rem .4rem; border-radius: 7px; line-height: 1.5rem; border: 1px solid;">GOLD</b>
+        Fully Customizable donation bar, animated and striped without any image :P
+      </li>
+
+      <li><b style="background-color: rgba(255, 215, 0, 0.51); padding: .1rem .4rem; border-radius: 7px; line-height: 1.5rem; border: 1px solid;">GOLD</b>
+       Charming colorful Charts, Doughnuts, Pies and candy Bars you choose or randomly picked for you.
+      </li>
+
+      <li><b style="background-color: rgba(255, 215, 0, 0.51); padding: .1rem .4rem; border-radius: 7px; line-height: 1.5rem; border: 1px solid;">GOLD</b>
+       Instant payment notifications for all <em>online</em> donations, no need for manually confirmation, all the hardwork is done for you :)
+      </li>
+
+      <li><b style="background-color: rgba(255, 215, 0, 0.51); padding: .1rem .4rem; border-radius: 7px; line-height: 1.5rem; border: 1px solid;">GOLD</b>
+       Payment processors logos in the donation page
+      </li>
+
+      <li><b style="background-color: rgba(255, 215, 0, 0.51); padding: .1rem .4rem; border-radius: 7px; line-height: 1.5rem; border: 1px solid;">GOLD</b>
+       Configurable decimal points upto 8 decimal points
+      </li>
+
+      <li><b style="background-color: rgba(255, 215, 0, 0.51); padding: .1rem .4rem; border-radius: 7px; line-height: 1.5rem; border: 1px solid;">GOLD</b>
+       Custom Currency setting for each payment processor.
+      </li>
+
+      <li>
+      ISO 4217 Currency codes.
+      </li>
+
+      <li>Donations Management.</li>
+      <li>Configurable donations goal and target amount</li>
+     </ul></ul><p id="naoardonate"></p>
+
+DOC;
+
+
