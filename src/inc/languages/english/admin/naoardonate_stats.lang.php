fbcaadfaee712b1d366a59dfaf07b225995114bf
CoderMe <srcii@coderme.com>
2017-09-07 09:43:35 +0200

    Updating: src/inc/languages/english/admin/naoardonate_stats.lang.php
    Signed-off-by: CoderMe <srcii@coderme.com>

---
+++ src/inc/languages/english/admin/naoardonate_stats.lang.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ *
+ * CoderMe Donation FREE
+ * Copyright (c) CoderMe.com, All Rights Reserved
+ *
+ * Website: https://coderme.com
+ * Home:    https://coderme.com/mybb-donation-plugin
+ * License: https://coderme.com/mybb-donation-plugin#license
+ * Version: 6.0.0
+ *
+ **/
+
+
+
+$l['naoardonate_stats_general'] = "General Statistics";
+$l['naoardonate_stats_general_desc'] = "quick and useful statistical information about all donations in general";
+$l['naoardonate_stats_members'] = "Top Members Donations";
+$l['naoardonate_stats_members_desc'] = "Top 21 confirmed members donations along with some info about them";
+$l['naoardonate_stats_guests'] = 'Top Guest Donations';
+$l['naoardonate_stats_guests_desc'] = "Top 21 confirmed guests donations along with some info about them (if available)";
+$l['naoardonate_stats_total_donations'] = "Total Donations";
+$l['naoardonate_stats_unconfirmed'] = "Unconfirmed";
+$l['naoardonate_stats_confirmed'] = "Confirmed";
+$l['naoardonate_stats_total'] = "Total";
+$l['naoardonate_stats_guests_donations'] = "Guests Donations";
+$l['naoardonate_stats_members_donations'] = "Members Donations";
+$l['naoardonate_stats_amount'] = "Donations Amount";
+$l['naoardonate_stats_highest'] = "Highest";
+$l['naoardonate_stats_average'] = "Average";
+$l['naoardonate_stats_least'] = "Least";
+$l['naoardonate_stats_topcurrencies'] = "Most Used Currencies";
+$l['naoardonate_stats_topamounts'] = "Most Chosen Donation Amounts";
+$l['naoardonate_stats_toppayment_methods'] = "Most Chosen Payment Methods";
+$l['naoardonate_stats_topgroups'] = "Most Donating Groups";
+$l['naoardonate_stats_times'] = "time(s)";
+$l['naoardonate_stats_notyet'] = 'Nothing yet :(';
+$l['naoardonate_stats_ogid'] = 'Original Group: {1}';
+
+
+
+?>
+
