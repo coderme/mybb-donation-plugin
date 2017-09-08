8f9330931e2034bec924d4f7db6d8ee0dc105863
CoderMe <srcii@coderme.com>
2017-09-08 14:54:32 +0200

    Updating: src/inc/languages/english/naoardonate_global.lang.php
    Signed-off-by: CoderMe <srcii@coderme.com>

---
+++ src/inc/languages/english/naoardonate_global.lang.php
@@ -0,0 +1,195 @@
+<?php
+
+/**
+ *
+ * CoderMe Donation FREE
+ * Copyright (c) CoderMe.com, All Rights Reserved
+ *
+ * Website: https://coderme.com
+ * Home:    https://coderme.com/mybb-donation-plugin
+ * License: https://coderme.com/mybb-donation-plugin#license
+ * Version: 6.0.0
+ *
+ **/
+
+
+
+# Admin: browse.php and stats.php | Front: donate.php
+$l['naoardonate_global_name'] = "Name";
+$l['naoardonate_global_guest'] = "Guest";
+$l['naoardonate_global_amount'] = "Amount";
+$l['naoardonate_global_payment_method'] = "Payment method";
+$l['naoardonate_global_ip'] = "IP Address";
+$l['naoardonate_global_extra'] = "Extra";
+$l['naoardonate_global_date'] = "Date";
+$l['naoardonate_global_you_confirmed'] = "you have confirmed this donation";
+$l['naoardonate_global_note_recieved'] = "a note sent by donor";
+$l['naoardonate_global_email_donor'] = "send email to this donor";
+
+
+# Admin: settings | Front: donate.php
+$l['naoardonate_global_currency'] = "Currency";
+
+
+# Admin: browse.php | Front donate.php
+$l['naoardonate_global_go'] = "Go";
+
+$l['naoardonate_global_nothing'] = 'Nothing to display :(';
+
+# Admin: meta, browse, stats
+$l['naoardonate_global_browse'] = "Browse Donors";
+$l['naoardonate_global_stats'] = "Donations&#39; Statistics";
+
+
+# Admin: setting,stats.php | Front: donate.php
+$l['naoardonate_global_currency_aed']    =   'AED  -  United Arab Emirates dirham';
+$l['naoardonate_global_currency_afn']    =   'AFN  -  Afghan afghani';
+$l['naoardonate_global_currency_all']    =   'ALL  -  Albanian lek';
+$l['naoardonate_global_currency_amd']    =   'AMD  -  Armenian dram';
+$l['naoardonate_global_currency_ang']    =   'ANG  -  Netherlands Antillean guilder';
+$l['naoardonate_global_currency_aoa']    =   'AOA  -  Angolan kwanza';
+$l['naoardonate_global_currency_ars']    =   'ARS  -  Argentine peso';
+$l['naoardonate_global_currency_aud']    =   'AUD  -  Australian dollar';
+$l['naoardonate_global_currency_awg']    =   'AWG  -  Aruban florin';
+$l['naoardonate_global_currency_azn']    =   'AZN  -  Azerbaijani manat';
+$l['naoardonate_global_currency_bam']    =   'BAM  -  Bosnia and Herzegovina convertible mark';
+$l['naoardonate_global_currency_bbd']    =   'BBD  -  Barbados dollar';
+$l['naoardonate_global_currency_bdt']    =   'BDT  -  Bangladeshi taka';
+$l['naoardonate_global_currency_bgn']    =   'BGN  -  Bulgarian lev';
+$l['naoardonate_global_currency_bhd']    =   'BHD  -  Bahraini dinar';
+$l['naoardonate_global_currency_bif']    =   'BIF  -  Burundian franc';
+$l['naoardonate_global_currency_bmd']    =   'BMD  -  Bermudian dollar';
+$l['naoardonate_global_currency_bnd']    =   'BND  -  Brunei dollar';
+$l['naoardonate_global_currency_bob']    =   'BOB  -  Boliviano';
+$l['naoardonate_global_currency_bov']    =   'BOV  -  Bolivian Mvdol';
+$l['naoardonate_global_currency_brl']    =   'BRL  -  Brazilian real';
+$l['naoardonate_global_currency_bsd']    =   'BSD  -  Bahamian dollar';
+$l['naoardonate_global_currency_btn']    =   'BTN  -  Bhutanese ngultrum';
+$l['naoardonate_global_currency_bwp']    =   'BWP  -  Botswana pula';
+$l['naoardonate_global_currency_byr']    =   'BYR  -  Belarusian ruble';
+$l['naoardonate_global_currency_bzd']    =   'BZD  -  Belize dollar';
+$l['naoardonate_global_currency_cad']    =   'CAD  -  Canadian dollar';
+$l['naoardonate_global_currency_cdf']    =   'CDF  -  Congolese franc';
+$l['naoardonate_global_currency_che']    =   'CHE  -  WIR Euro';
+$l['naoardonate_global_currency_chf']    =   'CHF  -  Swiss franc';
+$l['naoardonate_global_currency_chw']    =   'CHW  -  WIR Franc';
+$l['naoardonate_global_currency_clf']    =   'CLF  -  Unidad de Fomento';
+$l['naoardonate_global_currency_clp']    =   'CLP  -  Chilean peso';
+$l['naoardonate_global_currency_cnh']    =   'CNH  -  China Yuan (business/offshore)';
+$l['naoardonate_global_currency_cny']    =   'CNY  -  China Yuan Renminbi';
+$l['naoardonate_global_currency_cop']    =   'COP  -  Colombian peso';
+$l['naoardonate_global_currency_cou']    =   'COU  -  Unidad de Valor Real';
+$l['naoardonate_global_currency_crc']    =   'CRC  -  Costa Rican colon';
+$l['naoardonate_global_currency_cuc']    =   'CUC  -  Cuban convertible peso';
+$l['naoardonate_global_currency_cup']    =   'CUP  -  Cuban peso';
+$l['naoardonate_global_currency_cve']    =   'CVE  -  Cape Verde escudo';
+$l['naoardonate_global_currency_czk']    =   'CZK  -  Czech koruna';
+$l['naoardonate_global_currency_djf']    =   'DJF  -  Djiboutian franc';
+$l['naoardonate_global_currency_dkk']    =   'DKK  -  Danish krone';
+$l['naoardonate_global_currency_dop']    =   'DOP  -  Dominican peso';
+$l['naoardonate_global_currency_dzd']    =   'DZD  -  Algerian dinar';
+$l['naoardonate_global_currency_egp']    =   'EGP  -  Egyptian pound';
+$l['naoardonate_global_currency_ern']    =   'ERN  -  Eritrean nakfa';
+$l['naoardonate_global_currency_etb']    =   'ETB  -  Ethiopian birr';
+$l['naoardonate_global_currency_eur']    =   'EUR  -  Euro';
+$l['naoardonate_global_currency_fjd']    =   'FJD  -  Fiji dollar';
+$l['naoardonate_global_currency_fkp']    =   'FKP  -  Falkland Islands pound';
+$l['naoardonate_global_currency_gbp']    =   'GBP  -  Pound sterling';
+$l['naoardonate_global_currency_gel']    =   'GEL  -  Georgian lari';
+$l['naoardonate_global_currency_ghs']    =   'GHS  -  Ghanaian cedi';
+$l['naoardonate_global_currency_gip']    =   'GIP  -  Gibraltar pound';
+$l['naoardonate_global_currency_gmd']    =   'GMD  -  Gambian dalasi';
+$l['naoardonate_global_currency_gnf']    =   'GNF  -  Guinean franc';
+$l['naoardonate_global_currency_gtq']    =   'GTQ  -  Guatemalan quetzal';
+$l['naoardonate_global_currency_gyd']    =   'GYD  -  Guyanese dollar';
+$l['naoardonate_global_currency_hkd']    =   'HKD  -  Hong Kong dollar';
+$l['naoardonate_global_currency_hnl']    =   'HNL  -  Honduran lempira';
+$l['naoardonate_global_currency_hrk']    =   'HRK  -  Croatian kuna';
+$l['naoardonate_global_currency_htg']    =   'HTG  -  Haitian gourde';
+$l['naoardonate_global_currency_huf']    =   'HUF  -  Hungarian forint';
+$l['naoardonate_global_currency_idr']    =   'IDR  -  Indonesian rupiah';
+$l['naoardonate_global_currency_ils']    =   'ILS  -  Israeli new sheqel';
+$l['naoardonate_global_currency_inr']    =   'INR  -  Indian rupee';
+$l['naoardonate_global_currency_iqd']    =   'IQD  -  Iraqi dinar';
+$l['naoardonate_global_currency_irr']    =   'IRR  -  Iranian rial';
+$l['naoardonate_global_currency_isk']    =   'ISK  -  Icelandic króna';
+$l['naoardonate_global_currency_jmd']    =   'JMD  -  Jamaican dollar';
+$l['naoardonate_global_currency_jod']    =   'JOD  -  Jordanian dinar';
+$l['naoardonate_global_currency_jpy']    =   'JPY  -  Japanese yen';
+$l['naoardonate_global_currency_kes']    =   'KES  -  Kenyan shilling';
+$l['naoardonate_global_currency_kgs']    =   'KGS  -  Kyrgyzstani som';
+$l['naoardonate_global_currency_khr']    =   'KHR  -  Cambodian riel';
+$l['naoardonate_global_currency_kmf']    =   'KMF  -  Comoro franc';
+$l['naoardonate_global_currency_kpw']    =   'KPW  -  North Korean won';
+$l['naoardonate_global_currency_krw']    =   'KRW  -  South Korean won';
+$l['naoardonate_global_currency_kwd']    =   'KWD  -  Kuwaiti dinar';
+$l['naoardonate_global_currency_kyd']    =   'KYD  -  Cayman Islands dollar';
+$l['naoardonate_global_currency_kzt']    =   'KZT  -  Kazakhstani tenge';
+$l['naoardonate_global_currency_lak']    =   'LAK  -  Lao kip';
+$l['naoardonate_global_currency_lbp']    =   'LBP  -  Lebanese pound';
+$l['naoardonate_global_currency_lkr']    =   'LKR  -  Sri Lanka rupee';
+$l['naoardonate_global_currency_lrd']    =   'LRD  -  Liberian dollar';
+$l['naoardonate_global_currency_lsl']    =   'LSL  -  Lesotho loti';
+$l['naoardonate_global_currency_ltl']    =   'LTL  -  Lithuanian litas';
+$l['naoardonate_global_currency_lvl']    =   'LVL  -  Latvian lats';
+$l['naoardonate_global_currency_lyd']    =   'LYD  -  Libyan dinar';
+$l['naoardonate_global_currency_mad']    =   'MAD  -  Moroccan dirham';
+$l['naoardonate_global_currency_mdl']    =   'MDL  -  Moldovan leu';
+$l['naoardonate_global_currency_mga']    =   'MGA  -  Malagasy ariary';
+$l['naoardonate_global_currency_mkd']    =   'MKD  -  Macedonian denar';
+$l['naoardonate_global_currency_mmk']    =   'MMK  -  Myanma kyat';
+$l['naoardonate_global_currency_mnt']    =   'MNT  -  Mongolian tugrik';
+$l['naoardonate_global_currency_mop']    =   'MOP  -  Macanese pataca';
+$l['naoardonate_global_currency_mro']    =   'MRO  -  Mauritanian ouguiya';
+$l['naoardonate_global_currency_mur']    =   'MUR  -  Mauritian rupee';
+$l['naoardonate_global_currency_mvr']    =   'MVR  -  Maldivian rufiyaa';
+$l['naoardonate_global_currency_mwk']    =   'MWK  -  Malawian kwacha';
+$l['naoardonate_global_currency_mxn']    =   'MXN  -  Mexican peso';
+$l['naoardonate_global_currency_mxv']    =   'MXV  -  Mexican Unidad de Inversion (UDI)';
+$l['naoardonate_global_currency_myr']    =   'MYR  -  Malaysian ringgit';
+$l['naoardonate_global_currency_mzn']    =   'MZN  -  Mozambican metical';
+$l['naoardonate_global_currency_nad']    =   'NAD  -  Namibian dollar';
+$l['naoardonate_global_currency_ngn']    =   'NGN  -  Nigerian naira';
+$l['naoardonate_global_currency_nio']    =   'NIO  -  Cordoba oro';
+$l['naoardonate_global_currency_nok']    =   'NOK  -  Norwegian krone';
+$l['naoardonate_global_currency_npr']    =   'NPR  -  Nepalese rupee';
+$l['naoardonate_global_currency_nzd']    =   'NZD  -  New Zealand dollar';
+$l['naoardonate_global_currency_omr']    =   'OMR  -  Omani rial';
+$l['naoardonate_global_currency_pab']    =   'PAB  -  Panamanian balboa';
+$l['naoardonate_global_currency_pen']    =   'PEN  -  Peruvian nuevo sol';
+$l['naoardonate_global_currency_pgk']    =   'PGK  -  Papua New Guinean kina';
+$l['naoardonate_global_currency_php']    =   'PHP  -  Philippine peso';
+$l['naoardonate_global_currency_pkr']    =   'PKR  -  Pakistani rupee';
+$l['naoardonate_global_currency_pln']    =   'PLN  -  Polish złoty';
+$l['naoardonate_global_currency_pyg']    =   'PYG  -  Paraguayan guaraní';
+$l['naoardonate_global_currency_qar']    =   'QAR  -  Qatari riyal';
+$l['naoardonate_global_currency_ron']    =   'RON  -  Romanian new leu';
+$l['naoardonate_global_currency_rsd']    =   'RSD  -  Serbian dinar';
+$l['naoardonate_global_currency_rub']    =   'RUB  -  Russian rouble';
+$l['naoardonate_global_currency_rwf']    =   'RWF  -  Rwandan franc';
+$l['naoardonate_global_currency_sar']    =   'SAR  -  Saudi riyal';
+$l['naoardonate_global_currency_sbd']    =   'SBD  -  Solomon Islands dollar';
+$l['naoardonate_global_currency_scr']    =   'SCR  -  Seychelles rupee';
+$l['naoardonate_global_currency_sdg']    =   'SDG  -  Sudanese pound';
+$l['naoardonate_global_currency_sek']    =   'SEK  -  Swedish krona/kronor';
+$l['naoardonate_global_currency_sgd']    =   'SGD  -  Singapore dollar';
+$l['naoardonate_global_currency_shp']    =   'SHP  -  Saint Helena pound';
+$l['naoardonate_global_currency_sll']    =   'SLL  -  Sierra Leonean leone';
+$l['naoardonate_global_currency_sos']    =   'SOS  -  Somali shilling';
+$l['naoardonate_global_currency_srd']    =   'SRD  -  Surinamese dollar';
+$l['naoardonate_global_currency_ssp']    =   'SSP  -  South Sudanese pound';
+$l['naoardonate_global_currency_std']    =   'STD  -  São Tomé and Príncipe dobra';
+$l['naoardonate_global_currency_syp']    =   'SYP  -  Syrian pound';
+$l['naoardonate_global_currency_szl']    =   'SZL  -  Lilangeni';
+$l['naoardonate_global_currency_thb']    =   'THB  -  Thai baht';
+$l['naoardonate_global_currency_tjs']    =   'TJS  -  Tajikistani somoni';
+$l['naoardonate_global_currency_tmt']    =   'TMT  -  Turkmenistani manat';
+$l['naoardonate_global_currency_tnd']    =   'TND  -  Tunisian dinar';
+$l['naoardonate_global_currency_top']    =   'TOP  -  Tongan paʻanga';
+$l['naoardonate_global_currency_try']    =   'TRY  -  Turkish lira';
+$l['naoardonate_global_currency_ttd']    =   'TTD  -  Trinidad and Tobago dollar';
+$l['naoardonate_global_currency_twd']    =   'TWD  -  New Taiwan dollar';
+$l['naoardonate_global_currency_tzs']    =   'TZS  -  Tanzanian shilling';
+$l['naoardonate_global_currency_uah']    =   'UAH  -  Ukrainian hryvnia';
+$l['naoardonate_global_currency_ugx']    =   'UGX  -  Ugandan shilling';
+$l['naoardonate_global_currency_usd']    =   'USD  -  United States dollar';
