fb56bd9658c352dd70e4294667e93be245f51be8
CoderMe <srcii@coderme.com>
2017-09-24 03:19:13 +0200

    Updating: src/inc/languages/english/admin/naoardonate_global.lang.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/inc/languages/english/admin/naoardonate_global.lang.php
+++ src/inc/languages/english/admin/naoardonate_global.lang.php
@@ -197,3 +197,4 @@ $l['naoardonate_global_currency_ugx']    =   'UGX  -  Ugandan shilling';
 $l['naoardonate_global_currency_usd']    =   'USD  -  United States dollar';
 $l['naoardonate_global_currency_uyi']    =   'UYI  -  Uruguay Peso en Unidades Indexadas';
 $l['naoardonate_global_currency_uyu']    =   'UYU  -  Uruguayan peso';
+$l['naoardonate_global_currency_uzs']    =   'UZS  -  Uzbekistan som';
