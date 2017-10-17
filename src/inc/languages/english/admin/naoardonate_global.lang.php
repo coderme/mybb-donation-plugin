d3d3688867c0581ed1ce98c3a50718c636502f6c
CoderMe <srcii@coderme.com>
2017-10-17 20:58:03 +0200

    Updating: src/inc/languages/english/admin/naoardonate_global.lang.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/inc/languages/english/admin/naoardonate_global.lang.php
+++ src/inc/languages/english/admin/naoardonate_global.lang.php
@@ -204,3 +204,4 @@ $l['naoardonate_global_currency_vuv']    =   'VUV  -  Vanuatu vatu';
 $l['naoardonate_global_currency_wst']    =   'WST  -  Samoan tala';
 $l['naoardonate_global_currency_xaf']    =   'XAF  -  CFA franc BEAC';
 $l['naoardonate_global_currency_xcd']    =   'XCD  -  East Caribbean dollar';
+$l['naoardonate_global_currency_xof']    =   'XOF  -  CFA Franc BCEAO';
