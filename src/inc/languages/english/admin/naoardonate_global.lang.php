5329b3e84d4ed6334d76a162b3c716023c3b56aa
CoderMe <srcii@coderme.com>
2017-10-21 03:30:44 +0200

    Updating: src/inc/languages/english/admin/naoardonate_global.lang.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/inc/languages/english/admin/naoardonate_global.lang.php
+++ src/inc/languages/english/admin/naoardonate_global.lang.php
@@ -206,3 +206,4 @@ $l['naoardonate_global_currency_xaf']    =   'XAF  -  CFA franc BEAC';
 $l['naoardonate_global_currency_xcd']    =   'XCD  -  East Caribbean dollar';
 $l['naoardonate_global_currency_xof']    =   'XOF  -  CFA Franc BCEAO';
 $l['naoardonate_global_currency_xpf']    =   'XPF  -  CFP franc';
+$l['naoardonate_global_currency_yer']    =   'YER  -  Yemeni rial';
