98ecdf1685dc9d8b7eeec845c06964494cdb09b8
CoderMe <srcii@coderme.com>
2017-10-27 15:38:12 +0200

    Updating: src/inc/languages/english/naoardonate_global.lang.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/inc/languages/english/naoardonate_global.lang.php
+++ src/inc/languages/english/naoardonate_global.lang.php
@@ -208,3 +208,4 @@ $l['naoardonate_global_currency_yer']    =   'YER  -  Yemeni rial';
 $l['naoardonate_global_currency_zar']    =   'ZAR  -  South African rand';
 $l['naoardonate_global_currency_zmw']    =   'ZMW  -  Zambian kwacha';
 $l['naoardonate_global_currency_zwl']    =   'ZWL  -  Zimbabwe dollar';
+
