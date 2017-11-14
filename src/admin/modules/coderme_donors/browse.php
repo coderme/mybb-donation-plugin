e85adc50326b068ae7556554d4cc54da66d1ccf2
CoderMe <srcii@coderme.com>
2017-11-14 18:56:23 +0100

    Updating: src/admin/modules/coderme_donors/browse.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/admin/modules/coderme_donors/browse.php
+++ src/admin/modules/coderme_donors/browse.php
@@ -216,3 +216,4 @@ if($mybb->request_method == 'post'){
 			$lang->naoardonate_browse_inline_deleted = $lang->sprintf($lang->naoardonate_browse_inline_deleted, my_number_format(count($selected)));
 
 		}
+
