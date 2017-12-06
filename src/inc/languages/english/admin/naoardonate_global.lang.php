da65ba8c12d43429a70b9548dbf8e80af6140962
CoderMe <srcii@coderme.com>
2017-12-06 07:03:17 +0100

    Updating: src/inc/languages/english/admin/naoardonate_global.lang.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/inc/languages/english/admin/naoardonate_global.lang.php
+++ src/inc/languages/english/admin/naoardonate_global.lang.php
@@ -225,3 +225,4 @@ $l['naoardonate_global_currency_2c_pp_bk'] = '2checkout, Paypal and Bank/wire tr
 $l['naoardonate_global_currency_2c_wu_bk'] = '2checkout, Western Union and Bank/wire transfer ONLY';
 $l['naoardonate_global_currency_2c_bk'] = '2checkout and Bank/wire transfer ONLY';
 $l['naoardonate_global_currency_pp_wu_bk'] = 'PayPal, Western Union and Bank/wire transfer ONLY';
+$l['naoardonate_global_currency_pp_bk'] = 'PayPal and Bank/wire transfer ONLY';
