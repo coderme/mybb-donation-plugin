cdb80b25e6d5bb5061a06e9052439f0e0c982506
CoderMe <srcii@coderme.com>
2017-12-14 07:02:14 +0100

    Updating: src/inc/plugins/naoardonate.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/inc/plugins/naoardonate.php
+++ src/inc/plugins/naoardonate.php
@@ -229,3 +229,4 @@ function naoardonate_install()
           break;
 
           default:
+          $db->rename_column('naoardonate', 'amount', 'real_amount', "FLOAT UNSIGNED NOT NULL DEFAULT '0.00'");
