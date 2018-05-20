74ec9d38f1d9da0c4c03cc8df2e793b2a9e421ad
CoderMe <srcii@coderme.com>
2018-05-20 04:03:53 +0200

    Updating: src/inc/plugins/naoardonate.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/inc/plugins/naoardonate.php
+++ src/inc/plugins/naoardonate.php
@@ -307,3 +307,4 @@ function naoardonate_install()
                                    
     $settingsarray[] = array(
         'name' => 'naoardonate_payment_method',
+        'title' => $db->escape_string($lang->naoardonate_settings_payment_method),
