f2160110741cd5ddd71a3347b073b1a355e43c9d
CoderMe <srcii@coderme.com>
2018-07-09 22:07:26 +0200

    Updating: src/donate.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/donate.php
+++ src/donate.php
@@ -335,3 +335,4 @@ if( !in_array($mybb->input['action'], array('thank_you', 'top_donors'))){
 if($mybb->settings['naoardonate_info'] == 3 or $mybb->settings['naoardonate_info'] == 2 and $mybb->user['uid'] or $mybb->settings['naoardonate_info'] == 1 and !$mybb->user['uid']) {
 	if(empty($name) and empty($email) and $mybb->user['uid']){
 		$name = $mybb->user['username'];
+		$email = $mybb->user['email'];
