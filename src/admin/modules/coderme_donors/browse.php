d7859ebdce405fc9f65bf047351b22a56d942110
CoderMe <srcii@coderme.com>
2018-07-14 07:07:16 +0200

    Updating: src/admin/modules/coderme_donors/browse.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/admin/modules/coderme_donors/browse.php
+++ src/admin/modules/coderme_donors/browse.php
@@ -339,3 +339,4 @@ if( ! in_array( $mybb->input['action'], array('confirmed', 'unconfirmed'))){
 		$donor['dateline']= my_date($mybb->settings['dateformat'], $donor['dateline']).", ".my_date($mybb->settings['timeformat'], $donor['dateline']);
 		$table .= <<<TABLE_BODY
 		<tr id="donor1_$donor[did]">
+				<td class="align_center">$donor[name]</td>
