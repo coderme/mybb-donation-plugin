d1e769960a9a6ad7c2d9ea2a6cd20acb041a4872
CoderMe <srcii@coderme.com>
2018-07-24 06:31:04 +0200

    Updating: src/admin/modules/coderme_donors/stats.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/admin/modules/coderme_donors/stats.php
+++ src/admin/modules/coderme_donors/stats.php
@@ -343,3 +343,4 @@ $currencies_array = array(
 	# get top donors' groups
 	$query = $db->simple_select('naoardonate', 'ogid, COUNT(ogid) AS topgroups', 'confirmed = 1 AND real_amount > 0 GROUP BY ogid', array('order_by' => 'topgroups', 'order_dir' => 'DESC', 'limit' => 4));
 
+	while($row = $db->fetch_array($query))
