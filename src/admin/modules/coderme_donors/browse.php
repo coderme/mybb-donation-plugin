7fbf58976cecc1ca4ee4752a98b6fe2a65da4f38
CoderMe <srcii@coderme.com>
2018-08-19 05:30:17 +0200

    Updating: src/admin/modules/coderme_donors/browse.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/admin/modules/coderme_donors/browse.php
+++ src/admin/modules/coderme_donors/browse.php
@@ -359,3 +359,4 @@ TABLE_BODY;
 <input type="hidden" name="my_post_key" value="$mybb->post_code" />
 <input type="hidden" name="naoar_cookie" value="inlinemod_donor1" />
 <input type="hidden" name="naoar_referrer" value="index.php?module=coderme_donors{$sep}browse" />
+<span class="smalltext"><strong>$lang->naoardonate_browse_inline_moderation</strong></span>
