5bb7e5a9edc6aea430f7b7d326769262c8d9cf74
CoderMe <srcii@coderme.com>
2018-09-27 11:00:37 +0200

    Updating: src/donate.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/donate.php
+++ src/donate.php
@@ -381,3 +381,4 @@ if($mybb->settings['naoardonate_info'] == 3 or $mybb->settings['naoardonate_info
 eval('$captcha = "' . $templates->get('naoardonate_donate_captcha_v5') . '";');
 } elseif($captcha_valid){
 my_setcookie('imgstr', $mybb->input['imgstr'],'159');
+$captcha ='';
