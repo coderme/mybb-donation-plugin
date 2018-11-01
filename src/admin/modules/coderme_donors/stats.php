7ae312b582b00f4438f19e17e7379a826f9fe055
CoderMe <srcii@coderme.com>
2018-11-01 03:05:16 +0100

    Updating: src/admin/modules/coderme_donors/stats.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/admin/modules/coderme_donors/stats.php
+++ src/admin/modules/coderme_donors/stats.php
@@ -399,3 +399,4 @@ $currencies_array = array(
 	print '<div style="padding:27px;text-align:left">' . $lang->naoardonate_stats_notyet . '</div>';
 	}
 
+	print "</td><td><div class=\"coderme_div\">$lang->naoardonate_stats_amount :</div>";
