b6530ed6e07503779a21688df7d33107b9992c71
CoderMe <srcii@coderme.com>
2018-11-15 11:21:51 +0100

    Updating: src/admin/modules/coderme_donors/stats.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/admin/modules/coderme_donors/stats.php
+++ src/admin/modules/coderme_donors/stats.php
@@ -407,3 +407,4 @@ $currencies_array = array(
 			$lang->naoardonate_stats_highest : $highest </li>" : '') . ( $average ? "<li class=\"green\">$lang->naoardonate_stats_average : $average</li>" : '' ) . ( $least ? "<li class=\"navy\">
 			$lang->naoardonate_stats_least : $least</li>" : '') . "</ul>";
 	}
+	else
