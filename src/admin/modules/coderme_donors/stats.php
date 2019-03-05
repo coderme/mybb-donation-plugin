b839cc668325105849b9e16ff3115b730d101fb8
CoderMe <srcii@coderme.com>
2019-03-05 12:33:56 +0100

    Updating: src/admin/modules/coderme_donors/stats.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/admin/modules/coderme_donors/stats.php
+++ src/admin/modules/coderme_donors/stats.php
@@ -467,3 +467,4 @@ $currencies_array = array(
 
 		$query = $db->simple_select('naoardonate' ,'*' , 'real_amount > 0 AND uid != 0 AND confirmed=1',array('order_by' => 'real_amount', 'order_dir' => 'DESC', 'limit' => 21));
 
+		$table =<<<TABLE_HEAD
