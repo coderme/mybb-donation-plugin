eb9efbab251bff2c4f260b8f241b8b88f32dbcf6
CoderMe <srcii@coderme.com>
2019-03-06 12:56:01 +0100

    Updating: src/admin/modules/coderme_donors/browse.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/admin/modules/coderme_donors/browse.php
+++ src/admin/modules/coderme_donors/browse.php
@@ -469,3 +469,4 @@ TABLE_BODY;
 <span class="smalltext"><strong>$lang->naoardonate_browse_inline_moderation</strong></span>
 <select name="inline_action">
 		<option value="multidelete" >$lang->naoardonate_browse_delete</option>
+		<option value="multiconfirm" selected="selected" >$lang->naoardonate_browse_confirm</option>
