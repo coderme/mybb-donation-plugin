34c5f9cff060e2d782beacaac2bc529bdc817018
CoderMe <srcii@coderme.com>
2019-03-16 17:27:17 +0100

    Updating: src/inc/plugins/naoardonate.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/inc/plugins/naoardonate.php
+++ src/inc/plugins/naoardonate.php
@@ -473,3 +473,4 @@ function naoardonate_install()
         'name' => 'naoardonate_alert',
         'title' => $db->escape_string($lang->naoardonate_settings_unconfirmednotice),
         'description' => $db->escape_string($lang->naoardonate_settings_unconfirmednotice_desc),
+        'optionscode' => "radio
