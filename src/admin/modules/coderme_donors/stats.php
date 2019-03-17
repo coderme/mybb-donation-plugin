cb6f587dfd432c802a69fcf64f8901e4d28e125b
CoderMe <srcii@coderme.com>
2019-03-17 23:11:01 +0100

    Updating: src/admin/modules/coderme_donors/stats.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/admin/modules/coderme_donors/stats.php
+++ src/admin/modules/coderme_donors/stats.php
@@ -474,3 +474,4 @@ $currencies_array = array(
 		<th class="align_center">$lang->naoardonate_global_name</th>
 		<th class="align_center">$lang->naoardonate_global_amount</th>
 		<th class="align_center">$lang->naoardonate_global_payment_method</th>
+		<th class="align_center">$lang->naoardonate_global_ip</th>
