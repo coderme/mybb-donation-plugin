cd626d603ffa9e75ff137d7887b05897678dfe02
CoderMe <srcii@coderme.com>
2019-04-14 06:35:10 +0200

    Updating: src/inc/plugins/naoardonate.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/inc/plugins/naoardonate.php
+++ src/inc/plugins/naoardonate.php
@@ -488,3 +488,4 @@ disabled=$lang->naoardonate_settings_disabled
     
     if($mybb->settings['naoardonate_unban'])   {
          $naoardonate_unban = $mybb->settings['naoardonate_unban'];
+          }     else {
