5d733833084a17ded7687cf421aabc7405214a36
CoderMe <srcii@coderme.com>
2019-05-06 18:40:46 +0200

    Updating: src/inc/plugins/naoardonate.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/inc/plugins/naoardonate.php
+++ src/inc/plugins/naoardonate.php
@@ -501,3 +501,4 @@ disabled=$lang->naoardonate_settings_disabled
         'description' => $db->escape_string($lang->naoardonate_settings_unban_desc),
         'optionscode' => 'text',
         'value' => $db->escape_string($naoardonate_unban),
+        'disporder' => $c++,
