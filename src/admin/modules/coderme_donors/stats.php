c6d51fce09fe59c88b27c1a6d9f3396a2a12c38b
CoderMe <srcii@coderme.com>
2019-06-02 14:10:20 +0200

    Updating: src/admin/modules/coderme_donors/stats.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/admin/modules/coderme_donors/stats.php
+++ src/admin/modules/coderme_donors/stats.php
@@ -517,3 +517,4 @@ new PopupMenu('note_$donor[did]');
 		<td class="align_center">$donor[ip]</td>
 		<td class="align_center"><div class="coderme_info"><div>$email</div><div>$donor[ogid]</div><div>$note</div></div></td>
 		<td class="align_center">$donor[dateline]</td>
+</tr>
