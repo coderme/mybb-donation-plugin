3a16da72bc17e90b7c3efbd957096f9a879940eb
CoderMe <srcii@coderme.com>
2019-06-03 15:44:00 +0200

    Updating: src/admin/modules/coderme_donors/browse.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/admin/modules/coderme_donors/browse.php
+++ src/admin/modules/coderme_donors/browse.php
@@ -518,3 +518,4 @@ CODERME_INLINE;
 		if($donor['uid']) {
 
 		    $donor['name'] = "<a href=\"index.php?module=user{$sep}users&amp;action=edit&amp;uid=$donor[uid]\" target=\"_blank\">$donor[name]</a>" ;
+		    $donor['ogid'] = '<img src="./../images/naoar/group.gif" alt="" title="' .$lang->naoardonate_browse_ogid .  $groups[$donor['ogid']]['title'] . '" />';
