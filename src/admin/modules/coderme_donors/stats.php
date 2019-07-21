165af6241138abed53719020041dfe302d570b49
CoderMe <srcii@coderme.com>
2019-07-21 02:06:40 +0200

    Updating: src/admin/modules/coderme_donors/stats.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/admin/modules/coderme_donors/stats.php
+++ src/admin/modules/coderme_donors/stats.php
@@ -544,3 +544,4 @@ TABLE_BODY;
 
 
 	$query = $db->simple_select('naoardonate', '*', 'real_amount > 0 AND uid = 0 AND confirmed =1',array('order_by' => 'real_amount', 'order_dir'=> 'DESC', 'limit' => 21));
+
