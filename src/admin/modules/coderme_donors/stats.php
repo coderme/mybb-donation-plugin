86ef34a36d4eae4de5a19850cd4411b24940b838
CoderMe <srcii@coderme.com>
2019-08-11 09:34:01 +0200

    Updating: src/admin/modules/coderme_donors/stats.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/admin/modules/coderme_donors/stats.php
+++ src/admin/modules/coderme_donors/stats.php
@@ -554,3 +554,4 @@ TABLE_BODY;
 		<th class="align_center">$lang->naoardonate_global_payment_method</th>
 		<th class="align_center">$lang->naoardonate_global_ip</th>
 		<th class="align_center">$lang->naoardonate_global_extra</th>
+		<th class="align_center">$lang->naoardonate_global_date</th>
