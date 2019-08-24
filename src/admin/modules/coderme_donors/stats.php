f47445aeaf83cd138a540a9fe62b6ebefa852c99
CoderMe <srcii@coderme.com>
2019-08-24 22:59:07 +0200

    Updating: src/admin/modules/coderme_donors/stats.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/admin/modules/coderme_donors/stats.php
+++ src/admin/modules/coderme_donors/stats.php
@@ -562,3 +562,4 @@ TABLE_HEAD;
 		 while($donor = $db->fetch_array($query)){
 
 		 $donor['name']?$donor['name'] :$donor['name'] = 'Guest' ;
+		 $donor['dateline']= my_date($mybb->settings['dateformat'], $donor['dateline']).", ".my_date($mybb->settings['timeformat'], $donor['dateline']);
