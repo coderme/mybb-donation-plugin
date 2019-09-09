78da67cbfd1c49990aa1dfe1fe59b48385d2bcc7
CoderMe <srcii@coderme.com>
2019-09-09 11:44:45 +0200

    Updating: src/inc/plugins/naoardonate.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/inc/plugins/naoardonate.php
+++ src/inc/plugins/naoardonate.php
@@ -570,3 +570,4 @@ disabled=$lang->naoardonate_settings_disabled
         'optionscode' => 'textarea',
         'value' => $db->escape_string($naoardonate_reason),
         'disporder' => $c++,
+        'gid' => $gid
