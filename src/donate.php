6840390975b254212cd5bdff4589892ad78826be
CoderMe <srcii@coderme.com>
2019-09-21 02:23:12 +0200

    Updating: src/donate.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/donate.php
+++ src/donate.php
@@ -578,3 +578,4 @@ elseif ($mybb->settings['naoardonate_currency'] == 'Any')
 			. '</optgroup>';
 	$js_updatelist  .= " else if(a.payment_method.value == 'Paypal') {j.innerHTML = '<select name=\"currency\" class=\"w100\">$pp_currencies</select>'}";
     }
+
