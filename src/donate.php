662df64690e57f88db9aa445b283f7ed9bed5b43
CoderMe <srcii@coderme.com>
2019-10-15 12:16:07 +0200

    Updating: src/donate.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/donate.php
+++ src/donate.php
@@ -591,3 +591,4 @@ elseif ($mybb->settings['naoardonate_currency'] == 'Any')
 
     if ( in_array('Bank/Wire transfer', $accepted_payment_methods) )
     {
+	$currencyselect .='<optgroup label="' . $lang->sprintf( $lang->naoardonate_front_currencies_supported_by , 'Bank/Wire transfer') . '">'
