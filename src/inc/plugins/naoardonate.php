dac12cb6570582dd4ea5bd9f6d701f94f41d9e90
CoderMe <srcii@coderme.com>
2019-12-09 22:01:19 +0100

    Updating: src/inc/plugins/naoardonate.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/inc/plugins/naoardonate.php
+++ src/inc/plugins/naoardonate.php
@@ -628,3 +628,4 @@ disabled=$lang->naoardonate_settings_disabled
 
     if($mybb->settings['naoardonate_ifreached'])
         {
+        $naoardonate_ifreached = $mybb->settings['naoardonate_ifreached'];
