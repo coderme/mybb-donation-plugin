9a005075e799d803a33c072c74f3f43c60d64f53
CoderMe <srcii@coderme.com>
2019-12-13 05:35:34 +0100

    Updating: src/inc/plugins/naoardonate.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/inc/plugins/naoardonate.php
+++ src/inc/plugins/naoardonate.php
@@ -632,3 +632,4 @@ disabled=$lang->naoardonate_settings_disabled
         }
         elseif ($mybb->settings['teradonate_ifreached']) {
         $naoardonate_ifreached = $mybb->settings['teradonate_ifreached'];
+
