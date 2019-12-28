f0904a7340ec90a1a369b38764c7d09df95e82a1
CoderMe <srcii@coderme.com>
2019-12-28 13:01:28 +0100

    Updating: src/donate.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/donate.php
+++ src/donate.php
@@ -650,3 +650,4 @@ elseif($index_0  == 0 and $countofamount > 2){
 //   unset($amount_array[0]);
 
   $p_amount = '<select name="p_amount" onchange="custom()" class="w100">';
+  foreach ($amount_array as $k => $v){
