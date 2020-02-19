8efae1fdbee5707b46978e736109d0f09ce0e054
CoderMe <srcii@coderme.com>
2020-02-19 18:50:15 +0100

    Updating: src/donate.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/donate.php
+++ src/donate.php
@@ -716,3 +716,4 @@ if($mybb->settings['naoardonate_info_required'] and $aboutyou):
 	var ue = a.email;
 	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	if(ue.value == ''){
+	coderme_alert('{$lang->naoardonate_front_error_emailrequired}');
