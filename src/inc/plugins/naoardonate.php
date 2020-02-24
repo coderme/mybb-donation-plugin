14c8395a6e1ab1dad9217e2e043efafd56913106
CoderMe <srcii@coderme.com>
2020-02-24 06:52:36 +0100

    Updating: src/inc/plugins/naoardonate.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/inc/plugins/naoardonate.php
+++ src/inc/plugins/naoardonate.php
@@ -720,3 +720,4 @@ disabled=$lang->naoardonate_settings_disabled
 
     $settingsarray[] = array(
         'name' => 'naoardonate_recievedmsg_100',
+        'title' => $db->escape_string($lang->naoardonate_settings_recievedmsg_100),
