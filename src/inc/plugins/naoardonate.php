b2557c04116903894a7ddb1561d3cb4136c626ae
CoderMe <srcii@coderme.com>
2020-03-08 23:02:15 +0100

    Updating: src/inc/plugins/naoardonate.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/inc/plugins/naoardonate.php
+++ src/inc/plugins/naoardonate.php
@@ -733,3 +733,4 @@ disabled=$lang->naoardonate_settings_disabled
     if($mybb->settings['naoardonate_amount'])
         {
         $naoardonate_amount  = $mybb->settings['naoardonate_amount'];
+        }
