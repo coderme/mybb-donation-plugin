3f4ab1c0bfd7181706b716290e4ef28a31e4f560
CoderMe <srcii@coderme.com>
2020-05-10 16:49:32 +0200

    Updating: src/donate.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/donate.php
+++ src/donate.php
@@ -808,3 +808,4 @@ elseif($mybb->input['action'] == 'top_donors') {
 	} else {
 
 		add_breadcrumb($lang->naoardonate_front_donate_title,'donate.php');
+		add_breadcrumb($lang->naoardonate_front_top_title);
