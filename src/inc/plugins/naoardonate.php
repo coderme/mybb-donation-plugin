8ff2412c5f0aebc50553993555f42342fb76b353
CoderMe <srcii@coderme.com>
2020-08-01 02:51:14 +0200

    Updating: src/inc/plugins/naoardonate.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/inc/plugins/naoardonate.php
+++ src/inc/plugins/naoardonate.php
@@ -969,3 +969,4 @@ disabled=$lang->naoardonate_settings_disabled
         }
         elseif ($mybb->settings['teradonate_info_required']) {
         $naoardonate_info_required  = $mybb->settings['teradonate_info_required'];
+
