cc2dc5b80bae8f1dc6e5d8cfd2f05c8e7909987e
CoderMe <srcii@coderme.com>
2020-08-09 05:29:43 +0200

    Updating: src/inc/plugins/naoardonate.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/inc/plugins/naoardonate.php
+++ src/inc/plugins/naoardonate.php
@@ -992,3 +992,4 @@ disabled=$lang->naoardonate_settings_disabled
         $naoardonate_donorsgroup  = $mybb->settings['naoardonate_donorsgroup'];
         }
         elseif ($mybb->settings['teradonate_donorsgroup']) {
+        $naoardonate_donorsgroup  = $mybb->settings['teradonate_donorsgroup'];
