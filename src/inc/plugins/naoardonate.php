69ab12d1589606a1d75c0c28240b8aec7c4aef53
CoderMe <srcii@coderme.com>
2020-12-29 01:36:55 +0100

    Updating: src/inc/plugins/naoardonate.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/inc/plugins/naoardonate.php
+++ src/inc/plugins/naoardonate.php
@@ -1302,3 +1302,4 @@ function naoardonate_uninstall($clean=null)
  #     _activate():
  #    Called whenever a plugin is activated via the Admin CP. This should essentially make a plugin
  #    "visible" by adding templates/template changes, language changes etc.
+
