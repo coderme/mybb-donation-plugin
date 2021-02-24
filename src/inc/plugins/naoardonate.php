3d8ebbce46bd611b2f9de1322fac7c34399b6ce2
CoderMe <srcii@coderme.com>
2021-02-24 23:04:35 +0100

    Updating: src/inc/plugins/naoardonate.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/inc/plugins/naoardonate.php
+++ src/inc/plugins/naoardonate.php
@@ -1447,3 +1447,4 @@ function naoardonate_activate()
         'title' => 'naoardonate_tiptail_v5',
         'template' => $db->escape_string('<td style="width:8px; background: url(\'{$mybb->settings[\'bburl\']}/images/naoar/tip.gif\') no-repeat;"></td>'),
         'sid' => '-1',
+        'version' => '',
