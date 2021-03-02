8381132b3d504c4de67e9d76f774783b03623808
CoderMe <srcii@coderme.com>
2021-03-02 20:38:08 +0100

    Updating: src/inc/plugins/naoardonate.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/inc/plugins/naoardonate.php
+++ src/inc/plugins/naoardonate.php
@@ -1462,3 +1462,4 @@ function naoardonate_activate()
         </td>
             <td class="w70"><input type="text" name="name" value="{$name}" class="w80" /> <em>{$optional_required}</em>
         </td>
+
