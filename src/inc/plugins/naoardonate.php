d1fd4c7ccc48611d6a8908578e45720cdae350a2
CoderMe <srcii@coderme.com>
2021-03-10 07:56:58 +0100

    Updating: src/inc/plugins/naoardonate.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/inc/plugins/naoardonate.php
+++ src/inc/plugins/naoardonate.php
@@ -1484,3 +1484,4 @@ function naoardonate_activate()
         $templates_array[] = array(
         'title' => 'naoardonate_donate_currencies_row_v5',
         'template' => $db->escape_string('<tr>
+        <td><strong>{$lang->naoardonate_global_currency}:</strong>
