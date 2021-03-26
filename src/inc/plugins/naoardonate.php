e15cb1951cb208f18630ce5caca1b450ee6d7202
CoderMe <srcii@coderme.com>
2021-03-26 05:34:06 +0100

    Updating: src/inc/plugins/naoardonate.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/inc/plugins/naoardonate.php
+++ src/inc/plugins/naoardonate.php
@@ -1522,3 +1522,4 @@ function naoardonate_activate()
 
     $templates_array[] = array(
         'title' => 'naoardonate_donate_offline_bw_v5',
+        'template' => $db->escape_string('<fieldset class="w50 tleft" style="display: none;" id="{$payment_offline_id}">
