3b7b28d5903c94cd390cb3bc0424003a8e3a0959
CoderMe <srcii@coderme.com>
2021-05-09 12:40:01 +0200

    Updating: src/inc/plugins/naoardonate.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/inc/plugins/naoardonate.php
+++ src/inc/plugins/naoardonate.php
@@ -1605,3 +1605,4 @@ function naoardonate_activate()
 
         $templates_array[] = array(
         'title' => 'naoardonate_donate_captcha_v5',
+        'template' => $db->escape_string('<fieldset class="w50 tleft" >
