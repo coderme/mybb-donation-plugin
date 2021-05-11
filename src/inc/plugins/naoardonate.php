7ecae71664cb753f6194ca521655d16d3e66fa28
CoderMe <srcii@coderme.com>
2021-05-11 19:02:39 +0200

    Updating: src/inc/plugins/naoardonate.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/inc/plugins/naoardonate.php
+++ src/inc/plugins/naoardonate.php
@@ -1610,3 +1610,4 @@ function naoardonate_activate()
 <legend><strong>Image verification</strong></legend>
 <table cellspacing="0" cellpadding="{$theme[\'tablespace\']}" class="w100">
     <tr>
+        <td  style="padding-left:30%"><img src="captcha.php?action=regimage&amp;imagehash={$imagehash}" alt="{$lang->image_verification}" title="{$lang->naoardonate_front_refresh}" id="captcha_img" onmouseover="this.style.cursor=\'help\';"  onclick="return captcha.refresh();"  width="231" />
