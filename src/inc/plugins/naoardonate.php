025b2b7a37ed81d3ebde3184e3859959b81479b0
CoderMe <srcii@coderme.com>
2021-08-22 02:52:31 +0200

    Updating: src/inc/plugins/naoardonate.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/inc/plugins/naoardonate.php
+++ src/inc/plugins/naoardonate.php
@@ -1839,3 +1839,4 @@ jQuery("#coderme_alert").modal({ fadeDuration: 250, keepelement: true, zIndex: (
         </th>
         <th align="center" class="tcat"  width="15%"><span class="smalltext"><strong>{$lang->naoardonate_global_payment_method}</strong></span>
         </th>
+
