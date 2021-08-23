e2f483e7840609bfc9adc169c541b74ade3e3b8e
CoderMe <srcii@coderme.com>
2021-08-23 08:34:35 +0200

    Updating: src/inc/plugins/naoardonate.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/inc/plugins/naoardonate.php
+++ src/inc/plugins/naoardonate.php
@@ -1843,3 +1843,4 @@ jQuery("#coderme_alert").modal({ fadeDuration: 250, keepelement: true, zIndex: (
         <th align="center" class="tcat"  width="30%" ><span class="smalltext"><strong>{$lang->naoardonate_front_email}</strong></span>
         </th>
 
+        <th align="center" class="tcat"  width="25%"><span class="smalltext"><strong>{$lang->naoardonate_global_date}</strong></span>
