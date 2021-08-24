09222132a7f5344dc059c6aa38e427aa5cf63774
CoderMe <srcii@coderme.com>
2021-08-24 11:36:53 +0200

    Updating: src/inc/plugins/naoardonate.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/inc/plugins/naoardonate.php
+++ src/inc/plugins/naoardonate.php
@@ -1846,3 +1846,4 @@ jQuery("#coderme_alert").modal({ fadeDuration: 250, keepelement: true, zIndex: (
         <th align="center" class="tcat"  width="25%"><span class="smalltext"><strong>{$lang->naoardonate_global_date}</strong></span>
         </th>
     </tr>
+    {$donations}
