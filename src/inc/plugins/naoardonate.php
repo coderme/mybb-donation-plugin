4edf16bf55503c122ba62318676f4a31dd4b1d8c
CoderMe <srcii@coderme.com>
2021-08-31 01:22:57 +0200

    Updating: src/inc/plugins/naoardonate.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/inc/plugins/naoardonate.php
+++ src/inc/plugins/naoardonate.php
@@ -1864,3 +1864,4 @@ jQuery("#coderme_alert").modal({ fadeDuration: 250, keepelement: true, zIndex: (
 
     foreach($templates_array as $template):
         $db->insert_query("templates", $template);
+    endforeach;
