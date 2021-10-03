8f19c3865b321f175dbc59c3dd2e0c4fb1d6a30e
CoderMe <srcii@coderme.com>
2021-10-03 14:00:13 +0200

    Updating: src/inc/plugins/naoardonate.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/inc/plugins/naoardonate.php
+++ src/inc/plugins/naoardonate.php
@@ -1939,3 +1939,4 @@ function naoar_showdonatelinks()
 
     if($mybb->user['usergroup'] == 4 and $unconfirmed_donors > 0 and $mybb->settings['naoardonate_alert'] == 'notice') {
         require_once  MYBB_ROOT . $mybb->config['admin_dir']
+                                . '/inc/functions.php';
