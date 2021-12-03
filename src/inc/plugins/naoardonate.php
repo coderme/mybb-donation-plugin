bc618bb70b1497fe2542d692e8a1843b737b22c0
CoderMe <srcii@coderme.com>
2021-12-03 21:21:17 +0100

    Updating: src/inc/plugins/naoardonate.php
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/inc/plugins/naoardonate.php
+++ src/inc/plugins/naoardonate.php
@@ -2084,3 +2084,4 @@ function naoar_fixit()
         $mybb->input['upsetting']['naoardonate_unmovable'] = @implode(',', $mybb->input['upsetting']['naoardonate_unmovable']);
 
         $mybb->input['upsetting']['naoardonate_from'] = @implode(',', $mybb->input['upsetting']['naoardonate_from']);
+
